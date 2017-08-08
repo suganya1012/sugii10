03d2356a6d50da756c689ea04b28caf08a35f266
suganya1012 <30343772+suganya1012@users.noreply.github.com>
2017-08-08 11:35:04 +0530

    Create 21.c

---
+++ 21.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int k,r;
+    long int i=0l,j=1,f;
+
+    
+    printf("Enter the number range:");
+    scanf("%d",&r);
+
+    printf("FIBONACCI SERIES: ");
+    printf("%ld %ld",i,j); 
+
+    for(k=2;k<r;k++){
+         f=i+j;
+         i=j;
+         j=f;
+         printf(" %ld",j);
+    }
+  
+    return 0;
+}
+
