f4ffead3c86961865472b6eaf4b3139c42edf434
suganya1012 <30343772+suganya1012@users.noreply.github.com>
2017-08-08 11:52:06 +0530

    Create 25.c

---
+++ 25.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h> 
+ 
+void main()
+{
+int ifo=0;
+clrscr();
+while(ivo<=255)
+{
+printf("%d %c\n",ifo,ifo); 
+ivo++;
+}
+printf("\n press any key to proced");
+getch();
+}
