a78e9081811714cb5c874a9f4ddbea4821df9746
suganya1012 <30343772+suganya1012@users.noreply.github.com>
2017-08-08 11:08:19 +0530

    Create 15.c

---
+++ 15.c
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int p;
+ int g;
+ 
+ 
+ cout<<"please enter num"<<endl;
+cin>>a;
+cout<<"enter num pleaseeeeeeeeeee program by programology site abdullah moeen"<<endl;
+cin>>g;
+while(p<g){
+ 
+ if(a%2==0)
+ {
+ cout<<"even no. ="<<p<<endl;
+ 
+ }
+ p=p+1;
+}
+}
